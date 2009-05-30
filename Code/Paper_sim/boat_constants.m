9be68981b7c17a2de087de17037fa489eb3f113f
Bryant Mairs <bwmairs@ucsc.edu>
2009-05-29 22:23:07 -0700

    Moved the current CAD models as well as all documentation for this project into a single repository. Also rearranged the repo so that things are more organized.

---
+++ Code/Paper_sim/boat_constants.m
@@ -0,0 +1,49 @@
+%% Declare initial boat constants
+
+% Initial conditions
+
+e_0 = 0;                % Initial east position
+n_0 = 0;                % Initial north position
+psi_0 = 0;              % Initial heading
+T_step = 0.01;          % Simulation step time
+
+% Known constants
+
+rho_w = 1.03e3;         % Density of saltwater
+d_prop = .4572;         % Propeller diameter:18" (m)
+mass = 550;             % Boat mass (kg)
+wheelbase = 3.27;       % Distance from mid-keel to mid-rudder (m)
+m_battery = 108.86;     % Weight of ballast batteries (kg)
+d_battery = .3048;      % Distance of batteries from rotation axis (m)
+disp_vessel = 612.35;   % Vessel displacement:1350 lbs (kg)
+max_batt_angle = 1.309; % Maximum angle for the battery tray: 75 degrees(radians)
+rudder_angle_max = 1;   % Maximum angle of the rudder (radians)
+
+% Experimental constants
+% These are approximated constants. Need to be experimentally determined and then they will be known.
+
+K_t = 0.5;              % Thrust coefficient
+C_d_sea_surge = -340;   % Coefficient of drag for forward motion through water
+x_cg = .0508;           % Center of gravity relative to boat center: 1/6' (m)
+z_cg = .0508;           % Center of gravity relative to boat center: 1/6' (m)
+% Boat center is at 6.5' by the waterline by the axis of symmetry
+rudder_angle_dot_max = 10;   % Maximum rate of change of rudder angle (radians/s)
+proportional_band = .5;      % Proportional band of the rudder response (radians)
+delta_stall = .279;     % Stall angle for the rudder:16 degrees[from http://www.boatdesign.net/forums/sailboats/rudder-angle-effective-size-rudder-10724.html] (radians)
+rudder_area = .0945;    % Area of the rudder [see notes for calculation] (square meters)
+C_F = 1.15; % Coefficient of lift for the rudder (dimensionless)
+rudder_span = .4572;    % Distance to rudder along rotation axis (meters)
+x_cp = 3.4224;          % Distance from fixed-body origin along x-axis of center of pressure of the rudder (m)
+z_cp = .2032;           % Distance from fixed-body origin along z-axis of center of pressure of the rudder (m)
+t = 0.1;                % Thrust deduction number (dimensionless)
+Wt = 0.1;               % Taylor wake fraction
+
+%% Calculated physical constants
+% Moments of Inertia
+% Approximated using l=13,w=3,h=4 for a solid cuboid
+I_xx = mass/12*(3^2+4^2);
+I_yy = mass/12*(13^2+4^2);
+I_zz = mass/12*(13^2+3^2);
+
+Wf = 1 - Wt;            % Wake factor
+C_T = 6.4/(pi*rho_w*rudder_span*d_prop); % Thrust-loading coefficient
---
+++ Code/basic_model/sun_position_test.m
@@ -0,0 +1,22 @@
+%% Sun position tests
+% Sun position library was found online and this file is used to test the
+% library and confirm results. Results are confirmed against public 
+% experimental data: http://aa.usno.navy.mil/data/docs/AltAz.php
+
+%% Test 1:Santa Cruz at night
+% Result should be greater than 90 for zenith.
+% Azimuth should be pointing westward where sun sets
+
+% Government zenith/azimuth data can be found here:
+time = '27-May-2009 4:00:00'; % UTC (8pm local)
+
+location.longitude = -121.992874; 
+location.latitude = 36.955379; 
+location.altitude = 1000;
+
+% Should be more than 90 degrees zenith to be dark.
+sun = sun_position(time,location)
+
+% Convert to radians
+sun.azimuth = pi/180*sun.azimuth;
+sun.zenith = pi/180*sun.zenith;
