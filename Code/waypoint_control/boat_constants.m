8316a842d4d0a5ac3d5e60ef9959a501fca30a7d
Bryant Mairs <bwmairs@ucsc.edu>
2009-07-28 12:45:25 -0700

    - Modified boat_constants.m to increase the boat speed to above 2m/s so the simulation runs quicker. - Removed external forces besides drag. They will be added in again later once waypoint control is done. - Finished first step in adding waypoint control: use GPS coordinates to keep track of the boats position so we can specify waypoints in GPS coordinates.

--- Code/waypoint_control/boat_constants.m
+++ Code/waypoint_control/boat_constants.m
@@ -2,8 +2,10 @@
 
 % Initial conditions
 
-e_0 = 0;                % Initial east position
-n_0 = 0;                % Initial north position
+%e_0 = 0;                % Initial east position
+%n_0 = 0;                % Initial north position
+lat_0 = 36.80611;       % Initial latitude
+long_0 = -121.79639;    % Initial longitude
 phi_0 = 0;              % Initial heading
 v_0 = 0;                % Initial speed
 wind_speed_0 = 0;       % Initial wind speed
@@ -17,7 +19,7 @@ initial_charge = 20;    % Initial boat charge (J)
 
 rho_w = 1.03e3;         % Density of saltwater
 d_prop = .4572;         % Propeller diameter:18" (m)
-mass = 550;             % Boat mass (kg)
+mass = 1;             % Boat mass (kg)
 wheelbase = 3.27;       % Distance from mid-keel to mid-rudder (m)
 m_battery = 108.86;     % Weight of ballast batteries (kg)
 d_battery = .3048;      % Distance of batteries from rotation axis (m)
@@ -31,7 +33,7 @@ rudder_angle_max = 1;   % Maximum angle of the rudder (radians)
 wind_effect = 0.1;      % Wind effect coefficient. What % of the wind velocity affects the boat velocity
 current_effect = 0.1;   % Ocean current effect coefficient. What % of the current velocity affects the boat velocity
 K_t = 0.5;              % Thrust coefficient
-C_d_sea_surge = 340;    % Coefficient of drag for forward motion through water
+C_d_sea_surge = 200;    % Coefficient of drag for forward motion through water
 cg_x = .0508;           % Center of gravity relative to boat center: 1/6' (m)
 cg_z = .0508;           % Center of gravity relative to boat center: 1/6' (m)
 % Boat center is at 6.5' by the waterline by the axis of symmetry
--- Code/waypoint_control/render_boat.m
+++ Code/waypoint_control/render_boat.m
@@ -4,16 +4,17 @@
 function [] = render_boat(boat_position_x, boat_position_y, rudder_angle, boat_heading)
 
 gcf;
-axis([-500 500 -500 500]);
+axis([-200 200 -200 200]);
 axis('equal'); % Forces Matlab to render plot pixels as square
 hold on;       % Needed to render everything to same figure
 grid on;       % Render a nice grid
 
 % Define boat, rudder, and path points
-boat_x = 50*[-2.1 2.1 2.1 .75 0 -.75 -2.1];
-boat_y = 50*[-6 -6 0 3.9 6 3.9 0];
-rudder_x = 50*[-.15 .15 .15 -.15];
-rudder_y = 50*[-1.5 -1.5 0 0];
+scale = 10; % Define a multiplier for scaling the whole boat
+boat_x = scale*[-2.1 2.1 2.1 .75 0 -.75 -2.1];
+boat_y = scale*[-6 -6 0 3.9 6 3.9 0];
+rudder_x = scale*[-.15 .15 .15 -.15];
+rudder_y = scale*[-1.5 -1.5 0 0];
 
 % Generate some graphic objects for the path, rudder, and boat
 h_rudder = findobj('Tag','rudder');
@@ -42,7 +43,7 @@ new_rudder_x = cos(rudder_angle)*tmp_x - sin(rudder_angle)*tmp_y;
 new_rudder_y = sin(rudder_angle)*tmp_x + cos(rudder_angle)*tmp_y;
 
 % Move rudder into proper position with the boat.
-new_rudder_y = new_rudder_y - 6*50;
+new_rudder_y = new_rudder_y - 6*scale;
 
 % Perform boat rotation (rotates rudder and boat)
 tmp_x = new_boat_x;
