2d0aee43f55060a2623fdbc168afb3a911b8be94
Bryant Mairs <bwmairs@ucsc.edu>
2010-07-20 16:03:17 -0700

    - Removed some extra display sinks from basic_model - Added code_gen.mdl to be used for compiling the controller using RTW for running on real hardware   - Added make.m which copies the controller from the sim model into code_gen and then builds it.

---
+++ Code/basic_model/make.m
@@ -0,0 +1,13 @@
+%% This file compiles the simulation file for use on a dsPIC
+%% microcontroller.
+
+% This compilation flow follows the following steps:
+%  1) Copy the controller from Boat_sim.mdl into existing codegen.mdl
+%  replacing the dummy block there.
+%  2) Builds the model with the RTW Embedded Coder to generate a .hex file
+boat_sim = load_system('boat_sim');
+code_gen = load_system('code_gen');
+replace_block('code_gen', 'Name', 'Autonomous Controller', 'boat_sim/Autonomous Controller');
+close_system('code_gen', 1); % Close code_gen without saving
+close_system('boat_sim');
+rtwbuild('code_gen');
