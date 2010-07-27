576d815eb818691c09ecfac8c7982b09f2335d10
Bryant Mairs <bwmairs@ucsc.edu>
2010-07-26 20:27:35 -0700

    Added my own versions of the GPS and circular buffer code from SLUGS to the basic model. Code is currently untested. Alterations were mostly to remove the SLUGS dependencies and to add the date/time info to getGpsMainData().

---
+++ Code/basic_model/clib/gps.h
@@ -0,0 +1,102 @@
+ /*
+The MIT License
+
+Copyright (c) 2009 UCSC Autonomous Systems Lab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+#ifndef _GPS_H_
+#define _GPS_H_
+
+#ifdef __cplusplus
+       extern "C"{
+#endif
+       	
+#include "circBuffer.h"
+#include <stdlib.h>
+#include <string.h>
+
+typedef union{
+	unsigned char    chData[2];
+	unsigned short   usData;
+} tUnsignedShortToChar; 
+
+typedef union{
+	unsigned char    chData[2];
+	short   		 shData;
+} tShortToChar; 
+
+typedef union{
+	unsigned char   chData[4];
+	unsigned int   	uiData;
+} tUnsignedIntToChar; 
+
+typedef union{
+	unsigned char   chData[4];
+	int   			inData;
+} tIntToChar; 
+
+typedef union{
+	unsigned char   chData[4];
+	float   		flData;
+	unsigned short	shData[2];
+} tFloatToChar; 
+		   
+typedef struct tGpsData{
+	unsigned char	 		year;
+	unsigned char			month;
+	unsigned char			day;
+	unsigned char			hour;
+	unsigned char			min;
+	unsigned char			sec;	 
+	tFloatToChar 			lat;
+	tFloatToChar 			lon;
+	tFloatToChar 			height;
+	tUnsignedShortToChar	cog;
+	tUnsignedShortToChar	sog;
+	tUnsignedShortToChar	hdop;	
+	unsigned char			fix;
+	unsigned char 			sats;	
+	unsigned char			newValue;
+}tGpsData;
+
+#define TOKEN_SIZE	15
+  	
+void uartInit (void);
+void gpsSentenceConfig (void);
+void gpsFreqConfig (void);
+
+unsigned char hex2char (unsigned char halfhex);
+// void gpsRead (unsigned char* gpsChunk);
+void gpsInit (void);
+unsigned char gpsSeparate (unsigned char* outStream);
+void gpsParse (void);
+void getGpsMainData (float* data);
+float degMinToDeg (unsigned char degrees, float minutes);
+char gpSmbl (char symbl);
+void parseRMC (unsigned char* stream);
+void parseGGA (unsigned char* stream);
+
+#ifdef __cplusplus
+       }
+#endif
+       
+#endif /* _GPS_H_ */
