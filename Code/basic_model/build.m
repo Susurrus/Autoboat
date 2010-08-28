466f8b88ce454a3fdc1e6f0123f41c9650c8c914
Bryant Mairs <bwmairs@ucsc.edu>
2010-08-28 10:12:55 -0700

    Fixed shadowed variable warning.

--- Code/basic_model/build.m
+++ Code/basic_model/build.m
@@ -5,8 +5,8 @@
 %  1) Copy the controller from Boat_sim.mdl into existing codegen.mdl
 %  replacing the dummy block there.
 %  2) Builds the model with the RTW Embedded Coder to generate a .hex file
-boat_sim = load_system('boat_sim');
-code_gen = load_system('code_gen');
+load_system('boat_sim');
+load_system('code_gen');
 replace_block('code_gen', 'Name', 'Autonomous Controller', 'boat_sim/Autonomous Controller', 'noprompt');
 close_system('code_gen', 1); % Close code_gen without saving
 close_system('boat_sim');
