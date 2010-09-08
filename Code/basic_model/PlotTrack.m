e87ace57bd491a87e9e9596e6936daf37918037e
Bryant Mairs <bwmairs@ucsc.edu>
2010-09-08 11:30:44 -0700

    Updated plot titles to be more accurate.

--- Code/basic_model/PlotTrack.m
+++ Code/basic_model/PlotTrack.m
@@ -29,12 +29,12 @@ hold off;
 figure(2);clf;
 subplot(3,1,1);
 plot(rudder_command);
-title('Commanded rudder position');
+title('Rudder Commands');
 
 subplot(3,1,2);
 plot(throttle_command);
-title('Commanded throttle');
+title('Throttle Commands');
 
 subplot(3,1,3);
 plot(ballast_command);
-title('Commanded tray angle');
+title('Ballast Commands');
