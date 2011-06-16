033b5d7c140d2170ca054d29d5e42356b8583cad
Bryant Mairs <bwmairs@ucsc.edu>
2011-06-16 13:36:11 -0700

    Revert "Moved auxiliary code to Lib/*."
    This reverts commit 11354368d9bd4207c22f567c3da8a79079b36a4b.

--- Code/basic_model/boat_constants.m
+++ Code/basic_model/boat_constants.m
@@ -5,6 +5,7 @@
 % Initial latitude & longitude. This provides the baseline for the plant
 % to generate GPS data.
 initial_LL = [36.80611 -121.79639];
+phi_0 = 0;              % Initial heading (eastward positive from north)
 v_0 = 0;                % Initial speed
 battery_tray_angle = 0; % Initial battery tray angle
 T_step = 0.01;          % Simulation timestep
@@ -15,7 +16,7 @@ start_time = clock;
 
 % Physical input/output parameters
 
-
+OC2max = 49999; % Parameter used for calculating up-time & period for output compare 2.
 
 % Waypoint stuff
 % Waypoints are all defined within a local tangent plane in meters North,
@@ -64,6 +65,10 @@ sampling_waypoints = int32([
 
 % Known constants
 
+rho_w = 1.03e3;         % Density of saltwater
+d_prop = .4572;         % Propeller diameter:18" (m)
+mass = 550;             % Boat mass (kg)
+wheelbase = 3.27;       % Distance from mid-keel to mid-rudder (m)
 m_battery = 108.86;     % Weight of ballast batteries (kg)
 d_battery = .3048;      % Distance of batteries from rotation axis (m)
 disp_vessel = 612.35;   % Vessel displacement:1350 lbs (kg)
@@ -74,6 +79,8 @@ throttle_max = 1;       % Maximum throttle value (% of maximum amps)
 
 % Experimental constants
 
+K_t = 0.5;              % Thrust coefficient
+C_d_sea_surge = 340;    % Coefficient of drag for forward motion through water
 cg_x = .0508;           % Center of gravity relative to boat center: 1/6' (m)
 cg_z = .0508;           % Center of gravity relative to boat center: 1/6' (m)
 % Boat center is at 6.5' by the waterline by the axis of symmetry
@@ -82,3 +89,13 @@ throttle_dot_max = 1;   % Maximum rate of change of throttle (%/s)
 proportional_band = .5;      % Proportional band of the rudder response (radians)
 roll_period = 3;        % Periodicity of roll. (s)
 power_coefficients = [5.6667 9.6667 0]; % Power coefficients for use in the motor current draw calculations
+
+% L2+ constants
+TStar = single(4.5);
+IPStar = 0;
+InitialPoint = 0;
+Turn2Track = 0;
+MaxDwnPthStar = 1;
+tanIntercept = tan( 45*pi/180 );
+ISA_g     = 9.815;          % Gravity               (m/s/s)
+switchDistance = 4; % Meters before reaching a waypoint that you will then switch over to the next waypoint
