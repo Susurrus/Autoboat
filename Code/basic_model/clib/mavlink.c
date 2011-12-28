3022ce9c66f66f217aeb4eb6963de23631460dcc
Bryant Mairs <bwmairs@ucsc.edu>
2011-12-28 13:47:00 -0500

    Created a MAVLink driver for using MAVLink as the groundstation interface instead of my old custom one. Currently only in use by Tests/Groundstation.

---
+++ Code/basic_model/clib/mavlink.c
@@ -0,0 +1,54 @@
+#include "uart1.h"
+#include <stdint.h>
+#include <common/mavlink.h>
+ 
+static mavlink_system_t mavlink_system;
+
+/**
+ * This function creates a MAVLink heartbeat message with some basic parameters and
+ * caches that message (along with its size) in the module-level variables declared
+ * above. This buffer should be transmit at 1Hz back to the groundstation.
+ */
+void MavLinkInitHeartbeat(void) {
+ 
+	mavlink_system.sysid = 20;                              ///< ID 20 for this airplane
+	mavlink_system.compid = MAV_COMP_ID_SYSTEM_CONTROL;     ///< The component sending the message is the IMU, it could be also a Linux process
+	mavlink_system.type = MAV_TYPE_SURFACE_BOAT;            ///< This system is a surface vessel
+
+}
+
+/**
+ * This function transmits the cached heartbeat message via UART1.
+ */
+void MavLinkSendHeartbeat(void) {
+
+	uint8_t heartbeat_buf[MAVLINK_MAX_PACKET_LEN];
+	uint16_t heartbeat_len;
+	 
+	// Define this as a generic autopilot
+	uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
+	uint8_t system_mode = MAV_MODE_PREFLIGHT;
+	uint8_t system_status = MAV_STATE_STANDBY;
+	
+	// Initialize the required buffers
+	mavlink_message_t msg;
+
+	// Pack the message
+	mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, mavlink_system.type, autopilot_type, system_mode, 0, system_status);
+	 
+	// Copy the message to the send buffer
+	heartbeat_len = mavlink_msg_to_send_buffer(heartbeat_buf, &msg);
+	uart1EnqueueData(heartbeat_buf, (uint8_t)heartbeat_len);
+}
+
+void MavLinkSendStatus(void) {
+	mavlink_message_t msg;
+	
+	mavlink_msg_sys_status_pack(mavlink_system.sysid, mavlink_system.compid, &msg, 0, 0, 0, 500, 14000, 20000, 75, 20, 0, 0, 0, 0, 0);
+
+	uint8_t status_buf[MAVLINK_MAX_PACKET_LEN];
+	uint16_t status_len;
+	status_len = mavlink_msg_to_send_buffer(status_buf, &msg);
+	
+	uart1EnqueueData(status_buf, (uint8_t)status_len);
+}
