b0a3db3c80cfa9c4ad8b7a4528eb084f73e83c6d
Bryant Mairs <bwmairs@ucsc.edu>
2011-12-29 11:50:18 -0500

    Added a mavlink_gpd.mdl to Tests/Groundstation that includes the GPS_RAW_INT (#24) MAVLink message.

--- Code/basic_model/clib/mavlink.c
+++ Code/basic_model/clib/mavlink.c
@@ -9,11 +9,11 @@ static mavlink_system_t mavlink_system;
  * caches that message (along with its size) in the module-level variables declared
  * above. This buffer should be transmit at 1Hz back to the groundstation.
  */
-void MavLinkInitHeartbeat(void) {
+void MavLinkInit(void) {
  
-	mavlink_system.sysid = 20;                              ///< ID 20 for this airplane
-	mavlink_system.compid = MAV_COMP_ID_SYSTEM_CONTROL;     ///< The component sending the message is the IMU, it could be also a Linux process
-	mavlink_system.type = MAV_TYPE_SURFACE_BOAT;            ///< This system is a surface vessel
+	mavlink_system.sysid = 20;                              // ID 20 for this vehicle
+	mavlink_system.compid = MAV_COMP_ID_SYSTEM_CONTROL;     // The component sending the message is the IMU, it could be also a Linux process
+	mavlink_system.type = MAV_TYPE_SURFACE_BOAT;            // This system is a surface vessel
 
 }
 
@@ -45,6 +45,18 @@ void MavLinkSendStatus(void) {
 	mavlink_message_t msg;
 	
 	mavlink_msg_sys_status_pack(mavlink_system.sysid, mavlink_system.compid, &msg, 0, 0, 0, 500, 14000, 20000, 75, 20, 0, 0, 0, 0, 0);
+
+	uint8_t status_buf[MAVLINK_MAX_PACKET_LEN];
+	uint16_t status_len;
+	status_len = mavlink_msg_to_send_buffer(status_buf, &msg);
+	
+	uart1EnqueueData(status_buf, (uint8_t)status_len);
+}
+
+void MavLinkSendRawGps(void) {
+	mavlink_message_t msg;
+	
+	mavlink_msg_gps_raw_int_pack(mavlink_system.sysid, mavlink_system.compid, &msg, 1, 3, 35722, -78131, 0, 65535, 65535, 200, 1000, 4);
 
 	uint8_t status_buf[MAVLINK_MAX_PACKET_LEN];
 	uint16_t status_len;
