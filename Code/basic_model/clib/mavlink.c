64af94094dbf5498e6f8e5bb0789716f614e6ba3
Bryant Mairs <bwmairs@ucsc.edu>
2012-01-24 19:06:43 -0800

    Added MavLinkSendCurrentMission(void) so that the current mission can be transmit after a waypoint transition. Reformatted some of the code.

--- Code/basic_model/clib/mavlink.c
+++ Code/basic_model/clib/mavlink.c
@@ -8,20 +8,22 @@
 #include <stdint.h>
 #include <common/mavlink.h>
 //#include <sealion/mavlink.h>
- 
-static mavlink_system_t mavlink_system;
-static int packet_drops = 0;
+
+// Store a module-wide variable for common MAVLink system variables.
+static mavlink_system_t mavlink_system = {
+	20,
+	MAV_COMP_ID_SYSTEM_CONTROL,
+	MAV_TYPE_SURFACE_BOAT,
+	MAV_STATE_STANDBY,
+	MAV_MODE_PREFLIGHT,
+	MAV_MODE_FLAG_AUTO_ENABLED
+};
 
 // These variables are only used locally but are declared here module wise as all uses are orthogonal.
 static mavlink_status_t status;
 
-// Store the new size of the mission list. Used when receiving a new mission list to determine when
-// we're at the end.
-static mavlinkNewMissionListSize = -1;
-
-static uint8_t system_state = MAV_STATE_STANDBY;
-
-// Declare a character buffer here to simplify things
+// Declare a character buffer here to prevent continual allocation/deallocation of MAVLink buffers.
+static mavlink_message_t msg;
 static uint8_t buf[MAVLINK_MAX_PACKET_LEN];
 static uint16_t len;
 
@@ -30,26 +32,11 @@ static uint16_t len;
  * caches that message (along with its size) in the module-level variables declared
  * above. This buffer should be transmit at 1Hz back to the groundstation.
  */
-void MavLinkInit(void) {
- 
-	mavlink_system.sysid = 20;
-	mavlink_system.compid = MAV_COMP_ID_SYSTEM_CONTROL;
-	mavlink_system.type = MAV_TYPE_SURFACE_BOAT;
-
-}
-
-/**
- * This function transmits the cached heartbeat message via UART1.
- */
-void MavLinkSendHeartbeat(void) {
-	mavlink_message_t msg;
-	 
-	// Define this as a generic autopilot
-	uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
-	uint8_t system_mode = MAV_MODE_PREFLIGHT;
+void MavLinkSendHeartbeat(void)
+{
 
 	// Pack the message
-	mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, mavlink_system.type, autopilot_type, system_mode, 0, system_state);
+	mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, mavlink_system.type, MAV_AUTOPILOT_GENERIC, mavlink_system.mode, 0, mavlink_system.state);
 	 
 	// Copy the message to the send buffer
 	len = mavlink_msg_to_send_buffer(buf, &msg);
@@ -61,10 +48,17 @@ void MavLinkSendHeartbeat(void) {
  * It takes in a single argument for the computation load on the system. It should be between 0 and 100 and be in % of usage of the mainloop time.
  * TODO: Add info on the GPS sensor.
  */
-void MavLinkSendStatus(uint8_t load) {
-	mavlink_message_t msg;
+void MavLinkSendStatus(uint8_t load)
+{
+
+	// Declare that we have onboard sensors: 3D gyro, 3D accelerometer, 3D magnetometer, GPS
+	// And that we have the following controllers: yaw position, x/y position control, motor outputs/control.
+	// We reuse this variable for both the enabled and health status parameters as these are all assumed on and good.
+	// TODO: Use actual sensor health status for that portion of the status message.
+	uint32_t systemsPresent = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 5) |
+	                          (1 << 12) | (1 << 14) | (1 << 15);
 	
-	mavlink_msg_sys_status_pack(mavlink_system.sysid, mavlink_system.compid, &msg, 0xFFFF, 0xFFFF, 0xFFFF, ((uint16_t)load)*10, 14000, 20000, 75, 20, 0, 0, 0, 0, 0);
+	mavlink_msg_sys_status_pack(mavlink_system.sysid, mavlink_system.compid, &msg, systemsPresent, systemsPresent, systemsPresent, ((uint16_t)load)*10, 14000, 20000, 75, 20, 0, 0, 0, 0, 0);
 	
 	len = mavlink_msg_to_send_buffer(buf, &msg);
 	
@@ -76,8 +70,8 @@ void MavLinkSendStatus(uint8_t load) {
  * transmit it via MAVLink over UART1. This function should only be called when the GPS
  * data has been updated.
  */
-void MavLinkSendRawGps(uint32_t systemTime) {
-	mavlink_message_t msg;
+void MavLinkSendRawGps(uint32_t systemTime)
+{
 	
 	tGpsData gpsSensorData;
 	GetGpsData(&gpsSensorData);
@@ -90,7 +84,7 @@ void MavLinkSendRawGps(uint32_t systemTime) {
 
 	len = mavlink_msg_to_send_buffer(buf, &msg);
 	
-	//uart1EnqueueData(buf, (uint8_t)len);
+	uart1EnqueueData(buf, (uint8_t)len);
 }
 
 /**
@@ -99,15 +93,15 @@ void MavLinkSendRawGps(uint32_t systemTime) {
  * to ms for transmission.
  * Yaw should be in radians where positive is eastward from north.
  */
-void MavLinkSendAttitude(uint32_t systemTime, float yaw) {
-	mavlink_message_t msg;
+void MavLinkSendAttitude(uint32_t systemTime, float yaw)
+{
 
 	mavlink_msg_attitude_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
                                   systemTime*10, 0.0, 0.0, yaw, 0.0, 0.0, 0.0);
 
 	len = mavlink_msg_to_send_buffer(buf, &msg);
 	
-	//uart1EnqueueData(buf, (uint8_t)len);
+	uart1EnqueueData(buf, (uint8_t)len);
 }
 
 /**
@@ -115,7 +109,8 @@ void MavLinkSendAttitude(uint32_t systemTime, float yaw) {
  * Matlab, unpacks it, and ships it off over a MAVLink MAVLINK_MSG_ID_LOCAL_POSITION_NED
  * message.
  */
-void MavLinkSendLocalPosition(uint8_t *data) {
+void MavLinkSendLocalPosition(uint8_t *data)
+{
 	tUnsignedLongToChar systemTime;
 	tFloatToChar localPosX, localPosY, localVelX, localVelY;
 	
@@ -144,33 +139,31 @@ void MavLinkSendLocalPosition(uint8_t *data) {
 	localVelY.chData[2] = data[18];
 	localVelY.chData[3] = data[19];
 
-	mavlink_message_t msg;
-
 	mavlink_msg_local_position_ned_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
                                         systemTime.ulData, localPosX.flData, localPosY.flData, 0.0, localVelX.flData, localVelY.flData, 0.0);
 
 	len = mavlink_msg_to_send_buffer(buf, &msg);
 	
-	//uart1EnqueueData(buf, (uint8_t)len);
+	uart1EnqueueData(buf, (uint8_t)len);
 }
 
 /**
  * Transmits the current GPS position of the origin of the local coordinate frame that the North-East-Down
  * coordinates are all relative too. They should be in units of 1e-7 degrees.
  */
-void MavLinkSendGpsGlobalOrigin(int32_t latitude, int32_t longitude, int32_t altitude) {
-	mavlink_message_t msg;
+void MavLinkSendGpsGlobalOrigin(int32_t latitude, int32_t longitude, int32_t altitude)
+{
 
 	mavlink_msg_gps_global_origin_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
                                        latitude, longitude, altitude);
 
 	len = mavlink_msg_to_send_buffer(buf, &msg);
 	
-	//uart1EnqueueData(buf, (uint8_t)len);
+	uart1EnqueueData(buf, (uint8_t)len);
 }
 
-void MavLinkSendErrorsAndStatus(uint16_t status, uint16_t errors) {
-	//mavlink_message_t msg;
+void MavLinkSendErrorsAndStatus(uint16_t status, uint16_t errors)
+{
 
 	//mavlink_msg_status_and_errors_pack(mavlink_system.sysid, mavlink_system.compid, &msg, status, errors);
 
@@ -180,33 +173,52 @@ void MavLinkSendErrorsAndStatus(uint16_t status, uint16_t errors) {
 }
 
 /**
+ * Transmit the current mission index via UART1.
+ */
+void MavLinkSendCurrentMission(void)
+{
+	uint8_t currentMission;
+	
+	GetCurrentMission(&currentMission);
+	
+	mavlink_msg_mission_current_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &msg, currentMission);
+	len = mavlink_msg_to_send_buffer(buf, &msg);
+	uart1EnqueueData(buf, (uint8_t)len);
+}
+
+/**
 * @brief Receive communication packets and handle them. Should be called at the system sample rate.
 *
 * This function decodes packets on the protocol level and also handles
 * their value by calling the appropriate functions.
 */
-void MavLinkReceive(void) {
+void MavLinkReceive(void)
+{
+
+	// Store the new size of the mission list. Used when receiving a new mission list to determine when
+	// we're at the end.
+	static mavlinkNewMissionListSize = -1;
  
 	while(GetLength(&uart1RxBuffer) > 0) {
 		uint8_t c;
 		Read(&uart1RxBuffer, &c);
 		// Try to get a new message
-		mavlink_message_t msg;
-		if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
+		mavlink_message_t in_msg;
+		if(mavlink_parse_char(MAVLINK_COMM_0, c, &in_msg, &status)) {
 			// Handle message
 			mavlink_message_t out_msg;
  
-			switch(msg.msgid) {
+			switch(in_msg.msgid) {
 
 			// Begin reception of a new mission list. We first clear the mission list in preparation for new items and then
 			// request the first waypoint.
 			case MAVLINK_MSG_ID_MISSION_COUNT:
 				ClearMissionList();
 				// Record the number of incoming missions.
-				mavlinkNewMissionListSize = mavlink_msg_mission_count_get_count(&msg);
+				mavlinkNewMissionListSize = mavlink_msg_mission_count_get_count(&in_msg);
 				
 				mavlink_msg_mission_request_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg,
-					msg.sysid, msg.compid, 0);
+					in_msg.sysid, in_msg.compid, 0);
 				len = mavlink_msg_to_send_buffer(buf, &out_msg);
 				uart1EnqueueData(buf, (uint8_t)len);
 				break;
@@ -215,7 +227,7 @@ void MavLinkReceive(void) {
 			case MAVLINK_MSG_ID_MISSION_ITEM:
 			{
 				mavlink_mission_item_t newMission;
-				mavlink_msg_mission_item_decode(&msg, &newMission);
+				mavlink_msg_mission_item_decode(&in_msg, &newMission);
 				
 				// Make sure that they're coming in in the right order, and if they don't return an error in
 				// the acknowledgment response. This works because the mission list was cleared when we received
@@ -244,25 +256,25 @@ void MavLinkReceive(void) {
 						// If this was the last mission we were expecting, respond with an ACK confirming that we've successfully
 						// received the entire mission list.
 						if (missionAddStatus == mavlinkNewMissionListSize) {
-						mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, msg.sysid, msg.compid, MAV_MISSION_ACCEPTED);
-						len = mavlink_msg_to_send_buffer(buf, &out_msg);
-						uart1EnqueueData(buf, (uint8_t)len);
+							mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, in_msg.sysid, in_msg.compid, MAV_MISSION_ACCEPTED);
+							len = mavlink_msg_to_send_buffer(buf, &out_msg);
+							uart1EnqueueData(buf, (uint8_t)len);
 					
 						// Otherwise we just continue requesting for the subsequent missions.
 						} else {
 							mavlink_msg_mission_request_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg,
-								msg.sysid, msg.compid, newMission.seq + 1);
+								in_msg.sysid, in_msg.compid, newMission.seq + 1);
 							len = mavlink_msg_to_send_buffer(buf, &out_msg);
 							uart1EnqueueData(buf, (uint8_t)len);
 						}
 					// If we've run out of space before the last message, respond saying so.
 					} else {
-						mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, msg.sysid, msg.compid, MAV_MISSION_NO_SPACE);
+						mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, in_msg.sysid, in_msg.compid, MAV_MISSION_NO_SPACE);
 						len = mavlink_msg_to_send_buffer(buf, &out_msg);
 						uart1EnqueueData(buf, (uint8_t)len);
 					}
 				} else {
-					mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, msg.sysid, msg.compid, MAV_MISSION_INVALID_SEQUENCE);
+					mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, in_msg.sysid, in_msg.compid, MAV_MISSION_INVALID_SEQUENCE);
 					len = mavlink_msg_to_send_buffer(buf, &out_msg);
 					uart1EnqueueData(buf, (uint8_t)len);
 				}
@@ -275,7 +287,7 @@ void MavLinkReceive(void) {
 				uint8_t missionCount;
 				GetMissionCount(&missionCount);
 				mavlink_msg_mission_count_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg,
-					msg.sysid, msg.compid, missionCount);
+					in_msg.sysid, in_msg.compid, missionCount);
 				len = mavlink_msg_to_send_buffer(buf, &out_msg);
 				uart1EnqueueData(buf, (uint8_t)len);
 			}
@@ -284,7 +296,7 @@ void MavLinkReceive(void) {
 			// When a mission request message is received, respond with that mission information from the MissionManager
 			case MAVLINK_MSG_ID_MISSION_REQUEST:
 			{
-				uint8_t requestedMission = mavlink_msg_mission_request_get_seq(&msg);
+				uint8_t requestedMission = mavlink_msg_mission_request_get_seq(&in_msg);
 				Mission m;
 				uint8_t result;
 				GetMission(requestedMission, &m, &result);
@@ -292,7 +304,7 @@ void MavLinkReceive(void) {
 					uint8_t currMissionIndex;
 					GetCurrentMission(&currMissionIndex);
 					mavlink_msg_mission_item_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg,
-						msg.sysid, msg.compid, mavlink_msg_mission_request_get_seq(&msg),
+						in_msg.sysid, in_msg.compid, mavlink_msg_mission_request_get_seq(&in_msg),
 						m.refFrame, m.action, (requestedMission == currMissionIndex),
 						m.autocontinue, m.parameters[0], m.parameters[1], m.parameters[2], m.parameters[3], m.coordinates[0], m.coordinates[1], m.coordinates[2]);
 					len = mavlink_msg_to_send_buffer(buf, &out_msg);
@@ -305,7 +317,7 @@ void MavLinkReceive(void) {
 			case MAVLINK_MSG_ID_MISSION_CLEAR_ALL:
 				ClearMissionList();
 				mavlink_msg_mission_ack_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg,
-					msg.sysid, msg.compid, MAV_MISSION_ACCEPTED);
+					in_msg.sysid, in_msg.compid, MAV_MISSION_ACCEPTED);
 				len = mavlink_msg_to_send_buffer(buf, &out_msg);
 				uart1EnqueueData(buf, (uint8_t)len);
 				break;
@@ -313,14 +325,11 @@ void MavLinkReceive(void) {
 			// Allow for the groundstation to set the current mission. This requires a WAYPOINT_CURRENT response message agreeing with the received current message index.
 			case MAVLINK_MSG_ID_MISSION_SET_CURRENT:
 			{
-				uint8_t newCurrentMission = mavlink_msg_mission_set_current_get_seq(&msg);
+				uint8_t newCurrentMission = mavlink_msg_mission_set_current_get_seq(&in_msg);
 				SetCurrentMission(newCurrentMission);
 				
 				// Here we respond with the mission pulled via GetCurrentMission() so that we're sure we're sending the right mission #
-				GetCurrentMission(&newCurrentMission);
-				mavlink_msg_mission_current_pack(mavlink_system.sysid, MAV_COMP_ID_MISSIONPLANNER, &out_msg, newCurrentMission);
-				len = mavlink_msg_to_send_buffer(buf, &out_msg);
-				uart1EnqueueData(buf, (uint8_t)len);
+				MavLinkSendCurrentMission();
 			}
 				break;
 
@@ -335,7 +344,6 @@ void MavLinkReceive(void) {
 		// And get the next one
 	}
  
-	// Update global packet drops counter
-	packet_drops += status.packet_rx_drop_count;
+	status.packet_rx_drop_count;
 }
 
