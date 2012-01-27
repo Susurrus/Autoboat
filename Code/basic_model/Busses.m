321b30735c758e5fe3e3cb475f542360d6836e7b
Bryant Mairs <bwmairs@ucsc.edu>
2012-01-27 12:31:48 -0800

    code_gen.mdl now includes a new signal for storing the current timestep, the current reset state, and the system mode. I have also switched the HIL and HIL sensor mode button presses to be events that operate on this global data store. This allows for my C-code or other events to alter these states which wasn't possible before.

--- Code/basic_model/Busses.m
+++ Code/basic_model/Busses.m
@@ -120,6 +120,49 @@ MissionList.Alignment = -1;
 MissionList.Elements = elems;
 assignin('base', 'MissionList', MissionList)
 
+% Bus object: SystemState 
+clear elems;
+elems(1) = Simulink.BusElement;
+elems(1).Name = 'reset';
+elems(1).Dimensions = 1;
+elems(1).DimensionsMode = 'Fixed';
+elems(1).DataType = 'uint16';
+elems(1).SampleTime = -1;
+elems(1).Complexity = 'real';
+elems(1).SamplingMode = 'Sample based';
+elems(1).Min = [];
+elems(1).Max = [];
+
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'status';
+elems(2).Dimensions = 1;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'uint16';
+elems(2).SampleTime = -1;
+elems(2).Complexity = 'real';
+elems(2).SamplingMode = 'Sample based';
+elems(2).Min = [];
+elems(2).Max = [];
+
+elems(3) = Simulink.BusElement;
+elems(3).Name = 'time';
+elems(3).Dimensions = 1;
+elems(3).DimensionsMode = 'Fixed';
+elems(3).DataType = 'uint32';
+elems(3).SampleTime = -1;
+elems(3).Complexity = 'real';
+elems(3).SamplingMode = 'Sample based';
+elems(3).Min = [];
+elems(3).Max = [];
+
+SystemStatus = Simulink.Bus;
+SystemStatus.HeaderFile = '';
+SystemStatus.Description = sprintf('');
+SystemStatus.DataScope = 'Auto';
+SystemStatus.Alignment = -1;
+SystemStatus.Elements = elems;
+assignin('base', 'SystemStatus', SystemStatus)
+
 % Bus object: RudderCommand 
 clear elems;
 elems(1) = Simulink.BusElement;
