181412a3aac583adc6f498e39b6c4aab7c04c865
Bryant Mairs <bwmairs@ucsc.edu>
2012-06-22 19:46:30 -0700

    Updated Rudder.h to use shorthand aliases for its datatypes.

--- Code/basic_model/clib/Rudder.h
+++ Code/basic_model/clib/Rudder.h
@@ -1,13 +1,15 @@
 #ifndef _RUDDER_H_
 #define _RUDDER_H_
 
+#include <stdbool.h>
+
 #include "types.h"
 
 // Declaration of the relevant message structs used.
 struct RudderData {
 	tUnsignedShortToChar Position;
-	unsigned char PortLimit;
-	unsigned char StarboardLimit;
+	bool PortLimit;
+	bool StarboardLimit;
 };
 extern struct RudderData rudderDataStore;
 
@@ -15,13 +17,13 @@ extern struct RudderData rudderDataStore;
  * Retrieves the stored rudder data from the struct into
  * a packed uint8 array. Suitable for using in Matlab.
  */
-void GetRudderData(unsigned char *data);
+void GetRudderData(uint8_t *data);
 
 /**
  * Records the provided rudder data in a uint8 array
  * into a local tRudderData struct.
  */
-void SetRudderData(unsigned char *data);
+void SetRudderData(uint8_t *data);
 
 /**
  * Clears the stored rudder data to all zeros.
