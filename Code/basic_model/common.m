fb92a7b60b3ce9f04e7162485f2149b29d7bb359
Bryant Mairs <bwmairs@ucsc.edu>
2012-08-27 13:11:18 -0700

    Altered the GPS origin for the LTP to be on the corner of the south-eastern dock in Santa Cruz harbor.

--- Code/basic_model/common.m
+++ Code/basic_model/common.m
@@ -11,7 +11,7 @@ MaxPwmIn = .0025;
 
 % Initial GPS location
 % Stored as radians for a priori computations.
-ref_lla = [36.955; -122.002; 0] * pi / 180;
+ref_lla = [36.963765; -122.00191; 0] * pi / 180;
 % Stored as an int32 in 1e7 rads and 1e6 meters for dynamic computations.
 ref_lla_sense = int32([36.9543845 * 1e7; -122.0018435 * 1e7; 0 * 1e6]);
 
@@ -23,13 +23,3 @@ r = v * (1 - e2) / (1 - e2 * sin(ref_lla(1))^2);
 
 % The gain is calculated as normal along with a conversion from 1e-7 degrees to rads
 lla_ltp_gain = [r * (pi / 180 / 1e7); v * cos(ref_lla(1)) * (pi / 180 / 1e7); 1e-6];
-%latLonAlt = zeros(length(global_pos), 3, 'int32');
-%newRef = zeros(length(global_pos), 3, 'int32');
-%gains = zeros(length(global_pos), 3, 'double');
-%for i=1:length(global_pos)
-%    latLonAlt(i, :) = [global_pos(i, :)*1e7, 0];
-%    newRef(i, :) = ref_lla_sense;
-%    gains(i, :) = lla_ltp_gain;
-%end
-%diff = double(latLonAlt - newRef);
-%localPosition = single(diff .* gains);
