9619762055de966531501160932dd2a3332b20a1
Bryant <bwmairs@ucsc.edu>
2012-12-01 15:28:10 -0800

    Primary node:  * Refactored to use the new autonomous controller that doesn't do actuator output mapping or have ballast code.  * Move the RevoGS library to Libs where it should be.  * Removed the CommProtocol lib now that HIL is handled by a separate node  * and removed all vestigial HIL code from primary_node.mdl.

--- Code/Primary_node/clib/PrimaryNode.c
+++ Code/Primary_node/clib/PrimaryNode.c
@@ -1,12 +1,33 @@
 #include "Node.h"
-#include "CommProtocol.h"
 #include "MavlinkGlue.h"
 #include "DEE.h"
+#include "Acs300.h"
+#include "Uart1.h"
+#include "Uart2.h"
+#include "ecanFunctions.h"
+
+// This is the value of the BRG register for configuring different baud
+// rates. These BRG values have been calculated based on a 40MHz system clock.
+#define BAUD57600_BRG_REG 42
+#define BAUD115200_BRG_REG 21
 
 void PrimaryNodeInit(void)
 {
 	nodeId = CAN_NODE_PRIMARY_CONTROLLER;
-	cpInitCommunications();
+
+	// Initialize UART2 to 57600 for the Revolution GS.
+	Uart2Init(BAUD57600_BRG_REG);
+
+	// Initialize UART1 to 115200 for groundstation communications.
+	Uart1Init(BAUD115200_BRG_REG);
+
 	DataEEInitAndClear();
 	MavLinkInit();
+}
+
+void SendThrottleCommand(int16_t command)
+{
+    tCanMessage msg = {};
+    Acs300PackageWriteParam(&msg, ACS300_PARAM_CC, (uint16_t)command);
+    ecan1_buffered_transmit(&msg);
 }
