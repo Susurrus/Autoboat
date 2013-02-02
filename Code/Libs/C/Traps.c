c4a8805b1f224449cad1c5d8b2766425e5825c68
Bryant <bwmairs@ucsc.edu>
2013-02-01 17:49:42 -0800

    Traps lib now uses the FATAL_ERROR() macro in Node.h

--- Code/Libs/C/Traps.c
+++ Code/Libs/C/Traps.c
@@ -8,41 +8,33 @@
 
 #include <xc.h>
 
+#include "Node.h"
+
 void __attribute__((interrupt, no_auto_psv)) _OscillatorFail(void)
 {
 	INTCON1bits.OSCFAIL = 0;
-	TRISAbits.TRISA3 = 0;
-	LATAbits.LATA3 = 1;
-	while (1);
+	FATAL_ERROR();
 }
 
 void __attribute__((interrupt, no_auto_psv)) _AddressError(void)
 {
 	INTCON1bits.ADDRERR = 0;
-	TRISAbits.TRISA3 = 0;
-	LATAbits.LATA3 = 1;
-	while (1);
+	FATAL_ERROR();
 }
 void __attribute__((interrupt, no_auto_psv)) _StackError(void)
 {
 	INTCON1bits.STKERR = 0;
-	TRISAbits.TRISA3 = 0;
-	LATAbits.LATA3 = 1;
-	while (1);
+	FATAL_ERROR();
 }
 
 void __attribute__((interrupt, no_auto_psv)) _MathError(void)
 {
 	INTCON1bits.MATHERR = 0;
-	TRISAbits.TRISA3 = 0;
-	LATAbits.LATA3 = 1;
-	while (1);
+	FATAL_ERROR();
 }
 
 void __attribute__((interrupt, no_auto_psv)) _DMACError(void)
 {
 	INTCON1bits.DMACERR = 0;
-	TRISAbits.TRISA3 = 0;
-	LATAbits.LATA3 = 1;
-	while (1);
+	FATAL_ERROR();
 }
