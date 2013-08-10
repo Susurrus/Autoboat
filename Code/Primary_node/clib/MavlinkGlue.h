fb0cc2df4b5e4fc47efef8a1ff190a73422a837f
Bryant <bwmairs@ucsc.edu>
2013-08-10 14:17:47 -0700

    Fixed compilation with newer versions of MAVLink.h

--- Code/Primary_node/clib/MavlinkGlue.h
+++ Code/Primary_node/clib/MavlinkGlue.h
@@ -23,6 +23,8 @@
 #include "MissionManager.h"
 #include "primary_node.h"
 #include "Types.h"
+// Define M_PI_2 here for the MAVLink library as the XC16 doesn't provide this constant by default.
+#define M_PI_2 1.57079632679489661923
 #include "mavlink.h"
 
 // Events that trigger changes in the parameter protocol state machine.
