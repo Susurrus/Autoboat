718feda5b1bf1d07673333b3d341931cf32803ab
Bryant <bwmairs@ucsc.edu>
2013-10-18 09:43:07 -0700

    Doubled the size of the stack to 8k because of how much the compiler gave the HIL node.

--- Code/Libs/C/Stack.s
+++ Code/Libs/C/Stack.s
@@ -2,5 +2,5 @@
 ; Add this to any projects utilizing the dsPIC33Es where the warning "Taking the address of VARIABLE may require an extended pointer for this device" occurs.
 ; With this compiled into a program, you may ignore all the extended pointer warnings.
 
-.section non_eds_stack, stack, address(0x8000 - 4096) ; allocate the stack below 0x8000
-.space 4096
+.section non_eds_stack, stack, address(0x8000 - 8192) ; allocate the stack below 0x8000
+.space 8192
