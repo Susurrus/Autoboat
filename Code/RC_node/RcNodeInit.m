a6f3b89a86457dd82a9592186378652833ea1e9b
Bryant <bwmairs@ucsc.edu>
2014-02-13 12:03:31 -0800

    Revert "Moved all Input Capture code for the RC node into C code."
    This code was incomplete and should never have been completed in the first place. I'm not deferring any changes to the RC node as they're unnecessary at this stage.
    
    This reverts commit 9f3719c46f3f123d4cf259457b6fa8d1dfae40b7.

--- Code/RC_node/RcNodeInit.m
+++ Code/RC_node/RcNodeInit.m
@@ -1,4 +1,2 @@
-% The size (in seconds) of each simulator timestep
 T_step = 0.01;
-% The conversion factor for the IC uptime values to convert them to seconds.
-ic2time = 1/(80000000/2/256);
+MaxPwmIn = 0.0025;
