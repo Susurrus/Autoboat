97b3dc2ce57ef7fc92370e262f8e8cc90487f4ed
Bryant <bwmairs@ucsc.edu>
2015-02-12 18:01:27 -0800

    Copying MAVLink files should also update any pymavlink instances.

--- Code/Scripts/Bash/copy_to_qgroundcontrol.sh
+++ Code/Scripts/Bash/copy_to_qgroundcontrol.sh
@@ -5,6 +5,7 @@
 PROJ_DIR=~/Projects
 AUTOBOAT_DIR=$PROJ_DIR/Autoboat
 AUTOBOAT_MAVDIR=$AUTOBOAT_DIR/Code/Libs/MAVLink
+MAVLINK_DIR=$PROJ_DIR/mavlink
 QGROUNDCONTROL_DIR=$PROJ_DIR/qgroundcontrol
 QGROUNDCONTROL_MAVDIR=$QGROUNDCONTROL_DIR/libs/mavlink/include/mavlink/v1.0
 
@@ -16,3 +17,9 @@ git pull origin master
 # Overwrite QGroundControl's MAVLink files with this latest version
 rm -r $QGROUNDCONTROL_MAVDIR/*
 cp -r $AUTOBOAT_MAVDIR/* $QGROUNDCONTROL_MAVDIR
+
+# Copy the SeaSlug MAVLink definitions file to the MAVLink project
+cp $AUTOBOAT_MAVDIR/seaslug.xml $MAVLINK_DIR/message_definitions/v1.0/
+
+# And make sure we delete any old pymavlink versions of the SeaSlug message definitions
+rm $MAVLINK_DIR/pymavlink/dialects/v10/seaslug.py
