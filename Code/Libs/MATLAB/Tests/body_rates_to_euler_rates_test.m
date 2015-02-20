e577c833ea28f2a623dfb5b4b78ef3242120b9c0
Bryant <bwmairs@ucsc.edu>
2015-02-19 16:01:51 -0800

    Update tests and clarify docs for Euler rate calculations.

--- Code/Libs/MATLAB/Tests/body_rates_to_euler_rates_test.m
+++ Code/Libs/MATLAB/Tests/body_rates_to_euler_rates_test.m
@@ -1,5 +1,6 @@
 PI_2 = pi/2;
 PI_4 = pi/4;
+PI_6 = pi/6;
 
 % Easy null-test
 gyro_rates = [0; 0; 1]; % Body-frame yaw-rate of 1rad/s
@@ -22,6 +23,20 @@ exp_result = gyro_rates; % So we should get the same value
 act_result = body_rates_to_euler_rates(attitude, gyro_rates);
 assert(isequal(exp_result, act_result));
 
+% Test yaw rates when the vehicle is rolled 90 degrees
+gyro_rates = [0; 0; 1];
+attitude = [PI_2, 0, 0];
+exp_result = [0.0; -1.0; 0.0];
+act_result = body_rates_to_euler_rates(attitude, gyro_rates);
+assert(isequal(exp_result, act_result)); % FIXME: for floating point equality
+
+% Test yaw rates when the vehicle is rolled -90 degrees
+gyro_rates = [0; 0; 1];
+attitude = [-PI_2, 0, 0];
+exp_result = [0 1 0];
+act_result = body_rates_to_euler_rates(attitude, gyro_rates);
+assert(isequal(exp_result, act_result)); % FIXME: floating point equality
+
 % Now test when the vehicle is oriented 45 to port
 gyro_rates = [0; 0; 1];
 attitude = [0, 0, PI_4];
@@ -31,24 +46,20 @@ assert(isequal(exp_result, act_result));
 
 % Now test when the vehicle is pitched 45 degrees up the body-frame yaw
 % rate will be evenly split between the inertial roll and yaw axes.
+% FIXME: This test doesn't have a correct expected result
 gyro_rates = [0; 0; 1];
-attitude = [0, PI_4, 0];
-exp_result = gyro_rates;
-act_result = body_rates_to_euler_rates(attitude, gyro_rates)
+attitude = [0, PI_6, 0];
+exp_result = [0; 0; 0];
+act_result = body_rates_to_euler_rates(attitude, gyro_rates);
 assert(isequal(exp_result, act_result));
 
 % Now only roll the vehicle 45 degrees and test again
+% FIXME: This test doesn't have a correct expected result
 gyro_rates = [0; 0; 1];
 attitude = [PI_4, 0, 0];
-exp_result = gyro_rates;
-act_result = body_rates_to_euler_rates(attitude, gyro_rates)
+exp_result = [0; 0; 0];
+act_result = body_rates_to_euler_rates(attitude, gyro_rates);
 assert(isequal(exp_result, act_result));
-
-% And another one
-gyro_rates = [0; 0; 1];
-attitude = [0, PI_4, PI_2];
-exp_result = [];
-act_result = body_rates_to_euler_rates(attitude, gyro_rates)
 
 
 disp('SUCCESS: All tests passed for body_rates_to_euler_rates()');
--- Code/Libs/MATLAB/body_rates_to_euler_rates.m
+++ Code/Libs/MATLAB/body_rates_to_euler_rates.m
@@ -1,16 +1,16 @@
-function att_ypr_dot = body_rates_to_euler_rates(att_ypr, gyro_b)
+function att_dot = body_rates_to_euler_rates(att, gyro_b)
 %% This function rotates gyro data from body-axis to Euler-angle
 %#codegen
 % See https://www.princeton.edu/~stengel/MAE331Lecture9.pdf and
 % http://www.chrobotics.com/library/understanding-euler-angles for the
 % derivation.
-% att_ypr a 3-vector with the roll-pitch-yaw angle in rads (phi,theta,psi)
-% gyro_b the body-axis rates (referred to as p,q,r or omega_x,omega_y,omega_x)
-% att_ypr_dot The angular rates in the navigation frame (roll-rate,
-%             pitch-rate, yaw-rate)
+% att     a 3-vector with the roll-pitch-yaw angle in rads (phi,theta,psi)
+% gyro_b  the body-axis rates (referred to as p,q,r or omega_x,omega_y,omega_x)
+% att_dot The angular rates in the navigation frame (roll-rate,
+%         pitch-rate, yaw-rate)
 
-phi = att_ypr(1); % roll
-theta = att_ypr(2); % pitch
+phi = att(1); % roll
+theta = att(2); % pitch
 
 sin_phi = sin(phi);
 cos_phi = cos(phi);
@@ -21,4 +21,4 @@ C_b_to_n = [1 sin_phi*tan_theta cos_phi*tan_theta;
             0     cos_phi          -sin_phi;
             0 sin_phi*sec_theta cos_phi*sec_theta];
 
-att_ypr_dot = C_b_to_n * gyro_b;
+att_dot = C_b_to_n * gyro_b;
